6672fbdc897ad83fa31484b30e7f05e0ff790dd3
Sneara0 <snearaparvin.cse@gmail.com>
2026-05-18 10:34:10 +0600

    add login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/v1/auth/login", {
+      const response = await fetch("https://eventsphere-backend-seven.vercel.app/api/v1/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: finalEmail, password: finalPassword }),
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -33,7 +33,7 @@ export default function RegisterPage() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/v1/auth/register", {
+      const response = await fetch("https://eventsphere-backend-seven.vercel.app/api/v1/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
